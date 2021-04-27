f1dcb2fd03dd4618245b3ca765b0b862ed8666fe
Gjergj Kadriu <gjergjk71@gmail.com>
2021-04-27 16:16:12 +0200

    PUSH

--- src/models/index.js
+++ src/models/index.js
@@ -15,7 +15,10 @@ let sequelize;
 sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
-    logging: true
+    logging: true,
+    dialectOptions: {
+        ssl: true
+    }
 });
 
 
